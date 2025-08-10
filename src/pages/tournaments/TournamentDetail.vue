e192d7db936a8ddf4a2ad62ec76444c348b4c69b
dvelagui <dvelagui@gmail.com>
2025-08-10 14:04:03 -0500

    feat: add TournamentDetail component; implement template, script, and style sections

---
+++ src/pages/tournaments/TournamentDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+<h2>datails</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+console.log('TournamentDetail.vue loaded');
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
