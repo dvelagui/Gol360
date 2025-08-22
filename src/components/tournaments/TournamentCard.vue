aa96d342c3ac307b5489e66d843948f2901fa265
dvelagui <dvelagui@gmail.com>
2025-08-22 18:38:21 -0500

    feat(tournaments): update TournamentCard to display manager name instead of ID

--- src/components/tournaments/TournamentCard.vue
+++ src/components/tournaments/TournamentCard.vue
@@ -2,12 +2,20 @@
   <q-card class="q-pa-md">
     <div class="text-h6">{{ t.displayName }}</div>
     <div class="text-caption">{{ t.city }} • {{ t.type }}</div>
-    <div class="text-caption">Organizer: {{ t.managerId }}</div>
+    <div class="text-caption">Organizador: {{ getManagerName(t.managerId) }}</div>
     <q-btn flat color="primary" label="Ver" @click="$emit('open', t.id)" />
   </q-card>
 </template>
 
 <script setup lang="ts">
 import type { Tournament } from '@/types/auth'
+
+//hacer una funcion para traer el nombre del manager, teniendo su manager Id
+function getManagerName(managerId: string): Promise<string | null> {
+  const manager = { displayName: "Organizador", id: managerId }
+  return Promise.resolve(manager.displayName)
+}
+
+
 const { t } = defineProps<{ t: Tournament }>()
 </script>
